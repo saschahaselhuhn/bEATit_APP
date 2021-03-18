da95dfa71a98fd654a534ffec9dae3daea2e5a1c
saschahaselhuhn <sascha.haselhuhn@gmail.com>
2021-03-18 12:27:47 +0100

    🐛  fixing wrong code typing

--- components/cards/Cards.tsx
+++ components/cards/Cards.tsx
@@ -1,17 +1,21 @@
 import styles from "./Cards.module.css";
 
 export type Props = {
+  cardrecipesrc: string;
   cardrecipename: string;
   cardopenrecipesrc: string;
-  cardrecipesrc: string;
 };
 
-function RecipeCard(props: Props) {
+function RecipeCard({
+  cardrecipesrc,
+  cardrecipename,
+  cardopenrecipesrc,
+}: Props) {
   return (
     <div className={styles.recipecard}>
-      <img className={styles.recipecardimg} src={props.cardrecipesrc} />
-      <h1 className={styles.recipecardname}>{props.cardrecipename}</h1>
-      <img className={styles.recipecardopen} src={props.cardrecipesrc} />
+      <img className={styles.recipecardimg} src={cardrecipesrc} />
+      <h1 className={styles.recipecardname}>{cardrecipename}</h1>
+      <img className={styles.recipecardopen} src={cardopenrecipesrc} />
     </div>
   );
 }
