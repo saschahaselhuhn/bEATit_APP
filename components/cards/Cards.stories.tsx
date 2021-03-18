178e64b2cdf7e2f6292146f4efe03f65e719e3b6
saschahaselhuhn <sascha.haselhuhn@gmail.com>
2021-03-18 12:28:29 +0100

    ✨ Build Cards.stories.tsx with dummy data

---
+++ components/cards/Cards.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import Cards, { Props } from "./Cards";
+
+export default {
+  title: "Common/Cards",
+  component: Cards,
+} as Meta;
+
+const Template: Story<Props> = (args) => <Cards {...args} />;
+
+export const RecipeCard = Template.bind({});
+RecipeCard.args = {
+  cardrecipesrc: "string",
+  cardrecipename: "string",
+  cardopenrecipesrc: "string",
+};
