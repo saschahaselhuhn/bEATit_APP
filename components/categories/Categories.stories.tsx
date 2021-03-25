230cc1cc2c8b461ebfe4e2ceaad0fe11afa1a992
saschahaselhuhn <sascha.haselhuhn@gmail.com>
2021-03-25 10:38:44 +0100

    🐛  fixing issues after merge-problems

--- components/categories/Categories.stories.tsx
+++ components/categories/Categories.stories.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import Cards, { Props } from "./Categories";
+import Categories, { Props } from "./Categories";
 
 export default {
-  title: "Common/Cards",
-  component: Cards,
+  title: "Common/Category",
+  component: Categories,
 } as Meta;
 
-const Template: Story<Props> = (args) => <Cards {...args} />;
+const Template: Story<Props> = (args) => <Categories {...args} />;
 
-export const RecipeCard = Template.bind({});
-RecipeCard.args = {
-  recipeSrc: "./images/foodimages/paellamallorquina.jpg",
-  recipeName: "Paella Mallorquina",
-  openrecipeSrc: "./images/go.svg",
+export const CategoryCard = Template.bind({});
+CategoryCard.args = {
+  Src: "./images/foodimages/paellamallorquina.jpg",
+  Name: "Deutschland",
+  Link: "./images/go.svg",
 };
