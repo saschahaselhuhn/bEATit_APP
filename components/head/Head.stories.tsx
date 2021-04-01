e49c15de7218a0c8169180396c80f3360f71d9a4
saschahaselhuhn <77450338+saschahaselhuhn@users.noreply.github.com>
2021-04-01 11:36:48 +0200

    🚀  Add Static pages to the app (#13)

--- components/head/Head.stories.tsx
+++ components/head/Head.stories.tsx
@@ -1,17 +1,9 @@
 import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
-import Headerbar, { Props } from "./Head";
+import { Meta } from "@storybook/react/types-6-0";
+import Headerbar from "./Head";
 
 export default {
-  title: "Common/Head",
-  component: Headerbar,
+  title: "Common/headerBar",
 } as Meta;
 
-const Template: Story<Props> = (args) => <Headerbar {...args} />;
-
-export const Header = Template.bind({});
-Header.args = {
-  backBtn: "../images/backBtn.svg",
-  logoBtn: "../images/logowhite.svg",
-  menuBtn: "../images/menuBtn.svg",
-};
+export const headerBar = () => <Headerbar />;
