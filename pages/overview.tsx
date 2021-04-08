33ca4d908d12cf8f5bfd1f2f481374a0a8f09835
saschahaselhuhn <sascha.haselhuhn@gmail.com>
2021-04-08 22:38:17 +0200

    🐛  Fix Meta Viewport on every Page

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -34,6 +34,10 @@ export default function Home() {
       <Head>
         <title>Rezepte</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=0"
+        />
       </Head>
       <Headerbar onBurgerClick={() => setShowMenu(true)} />
       <Categoryhead
