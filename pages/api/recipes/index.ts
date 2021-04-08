184fcc074a0ff7f564c58bfea22f6c52f8b7764a
saschahaselhuhn <77450338+saschahaselhuhn@users.noreply.github.com>
2021-04-08 16:17:23 +0200

    ⚡️ fetch category-cards by ip (#25)

--- pages/api/recipes/index.ts
+++ pages/api/recipes/index.ts
@@ -2,10 +2,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { foodCategory } = req.query;
+
   const response = await fetch(
     "https://beatit.sascha-haselhuhn.com/wp-json/custom-routes/v1/beatit-database/"
   );
 
   const recipes = await response.json();
-  res.status(200).json(recipes);
+
+  const filteredRecipes = foodCategory
+    ? recipes.filter((recipe) => recipe.foodcategory.includes(foodCategory))
+    : recipes;
+  res.status(200).json(filteredRecipes);
 };
--- utils/api.ts
+++ utils/api.ts
@@ -7,8 +7,10 @@ export type APIRecipes = {
   featured_image: FeaturedImage;
   beatit_zutaten: Zutat[];
   beatit_steps: Schritt[];
+  foodcategory: string[];
 };
-
+/* memo an mich selbst: recipe.foodcategory.[]
+ */
 export type Schritt = {
   stepname: string;
   stepindstruction: string;
@@ -31,8 +33,12 @@ async function fetchURL<T>(url: string): Promise<T> {
   return await response.json();
 }
 
-export async function getRecipes(): Promise<APIRecipes[]> {
-  return await fetchURL<APIRecipes[]>("/api/recipes");
+export async function getRecipes(foodCategory?: string): Promise<APIRecipes[]> {
+  let url = "/api/recipes";
+  if (foodCategory) {
+    url += `?foodCategory=${foodCategory}`;
+  }
+  return await fetchURL<APIRecipes[]>(url);
 }
 
 export async function getRecipe(id: string): Promise<APIRecipes> {
