f9422c895e4d181a9b841058ac1196f86a28f352
saschahaselhuhn <sascha.haselhuhn@gmail.com>
2021-04-08 22:28:23 +0200

    🐛  fix meta-data

--- pages/[id]/steps.tsx
+++ pages/[id]/steps.tsx
@@ -32,7 +32,10 @@ export default function Recipe() {
       <Head>
         <title>{recipe.title}</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=0"
+        />
       </Head>
       <main>
         <Headerbar onBurgerClick={() => setShowMenu(true)} />
