a1b39c5cb2d9a6771727bae7a652d4b142eaca2a
saschahaselhuhn <sascha.haselhuhn@gmail.com>
2021-04-09 11:48:05 +0200

    ✨ Adding Category Bar

--- components/categories/Categories.tsx
+++ components/categories/Categories.tsx
@@ -2,19 +2,61 @@ import styles from "./Categories.module.css";
 
 export type Props = {
   Src: string;
-  Name: string;
-  Link: string;
+  LinkDeutsch: string;
+  LinkSpanisch: string;
+  LinkItalienisch: string;
+  LinkAmerikanisch: string;
 };
 
-function Categories({ Src, Name, Link }: Props) {
+function Categories({
+  Src,
+  LinkDeutsch,
+  LinkSpanisch,
+  LinkItalienisch,
+  LinkAmerikanisch,
+}: Props) {
   return (
-    <div className={styles.category}>
-      <a href={Link}>
-        <img className={styles.image} src={Src} />
-        <div className={styles.name}>
-          <h3>{Name}</h3>
+    <div className={styles.gridContainer}>
+      <div className={styles.deutsch}>
+        <div className={styles.category}>
+          <a href={LinkDeutsch}>
+            <img className={styles.image} src={Src} />
+            <div className={styles.name}>
+              <h3>Deutschland</h3>
+            </div>
+          </a>
         </div>
-      </a>
+      </div>
+      <div className={styles.spanisch}>
+        <div className={styles.category}>
+          <a href={LinkSpanisch}>
+            <img className={styles.image} src={Src} />
+            <div className={styles.name}>
+              <h3>Spanien</h3>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className={styles.italienisch}>
+        <div className={styles.category}>
+          <a href={LinkItalienisch}>
+            <img className={styles.image} src={Src} />
+            <div className={styles.name}>
+              <h3>Italien</h3>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className={styles.amerikanisch}>
+        <div className={styles.category}>
+          <a href={LinkAmerikanisch}>
+            <img className={styles.image} src={Src} />
+            <div className={styles.name}>
+              <h3>U.S.A.</h3>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
